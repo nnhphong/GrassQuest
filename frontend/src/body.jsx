86d273b449a7d957205f330952c8ad7df7078c70
_Fushigidane_ <84942946+Fushigidane-Snom@users.noreply.github.com>
2024-05-18 01:57:30 -0400

    Stuff

--- frontend/src/body.jsx
+++ frontend/src/body.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
 function Body({setView}) {
   return (
-    <>
+    <div className="bg-[url(./assets/test.jpg)] h-dvh bg-cover">
         <h1 className = "px-2 py-10 text-center text-7xl">Hello User!</h1>
         <h2 className = "px-2 py-2 text-center text-wrap text-3xl">Want to start an adventure?</h2>
         <div className="flex justify-center my-2">
@@ -9,7 +9,12 @@ function Body({setView}) {
               setView("ingame");
             }}>Navigate</button>
         </div>
-    </>
+        <div className="flex justify-center my-2">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-4 px-8 rounded" onClick={()=>{
+              setView("instructions");
+            }}>Instructions</button>
+        </div>
+    </div>
   )
 }
 
--- frontend/src/instructions.jsx
+++ frontend/src/instructions.jsx
@@ -1,10 +1,10 @@
 // eslint-disable-next-line react/prop-types
 function Instructions() {
     return (
-      <>
+      <div className="bg-[url(./assets/test.jpg)] h-dvh bg-cover">
         <h1 className = "px-2 py-10 text-center text-7xl">Instructions</h1>
         <p className = "px-2 py-2 text-center text-wrap text-3xl">To begin, press the button on the home page that says &quot;navigate&quot; to start the game. You can upload an image from your device to prove that you been there. After evaluation, you will be awarded points to level up on the leaderboard.</p>
-      </>
+      </div>
     )
   }
   
