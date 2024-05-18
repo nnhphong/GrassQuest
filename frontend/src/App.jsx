f1e3b427000ab4b1b3e3ad4795f4137085555494
_Fushigidane_ <84942946+Fushigidane-Snom@users.noreply.github.com>
2024-05-18 01:04:40 -0400

    Add game

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import Navbar from "./Navbar"
+import Game from "./Game"
+import { useState } from 'react';
 
 function App() {
+  const [view, setView] = useState("ingame");
+
   return (
     <>
-      <Navbar />
-      <div class="text-3xl font-bold underline">Hello, World!</div>
+      <Navbar setView={setView}/>
+      {
+        view == "home" ? 
+          <div className="text-3xl font-bold underline">Hello, World!</div> 
+        : view == "ingame" ? 
+          <Game setView={setView}/>
+
+        : <div></div>
+        }
     </>
   )
 }
--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,12 +1,12 @@
 import Menu from "./assets/Menu.svg"
 
-function Navbar() {
+function Navbar({setView}) {
   return (
     <>
        <header className="bg-slate-900 py-3 lg:py-4 sticky z-[9999] top-0 px-2" id="home">
             <div className="container px-4 mx-auto flex items-center">
                 <div className="flex justify-between items-center">
-                    <h1 className="text-white font-bold text-4xl py-2">Quests™</h1>
+                    <h1 className="text-white font-bold text-4xl py-2"  onClick={() => setView("home")}>Quests™</h1>
                 </div>
                 <div className="text-white flex flex-row ml-auto mt-0">
                     <img src={Menu}/>
