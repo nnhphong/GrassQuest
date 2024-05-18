9944b724ea8dad0ca4af33c9aa68d9ca3e3a60d3
Li Xing Yin <lixingyin@Lis-Air-2.wlu.ca>
2024-05-18 01:03:49 -0400

    added body

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar"
+import Body from "./body"
 
 function App() {
   return (
     <>
       <Navbar />
+      <Body />
       <div class="text-3xl font-bold underline">Hello, World!</div>
     </>
   )
---
+++ frontend/src/body.jsx
@@ -0,0 +1,13 @@
+function Body() {
+  return (
+    <>
+        <h1 class = "px-2 py-10 text-center text-7xl">Hello User!</h1>
+        <h2 class = "px-2 py-5 text-center text-wrap text-3xl">Want to start an adventure?</h2>
+        <div class="flex justify-center">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-4 px-8 rounded">Navigate</button>
+        </div>
+    </>
+  )
+}
+
+export default Body
