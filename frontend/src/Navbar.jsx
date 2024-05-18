cdf078d1acc7be908e581a5f2a32422df2583f3a
_Fushigidane_ <84942946+Fushigidane-Snom@users.noreply.github.com>
2024-05-17 23:59:44 -0400

    Finish header

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,13 +1,15 @@
+import Menu from "./assets/Menu.svg"
+
 function Navbar() {
   return (
     <>
-       <header className="bg-slate-900 py-2 lg:py-4 sticky z-[9999] top-0" id="home">
-            <div className="container px-4 mx-auto lg:flex lg:items-center">
+       <header className="bg-slate-900 py-3 lg:py-4 sticky z-[9999] top-0 px-2" id="home">
+            <div className="container px-4 mx-auto flex items-center">
                 <div className="flex justify-between items-center">
                     <h1 className="text-white font-bold text-4xl py-2">Quests™</h1>
                 </div>
-                <div className="text-white lg:flex flex-col lg:flex-row lg:ml-auto mt-3 lg:mt-0">
-                    Hello
+                <div className="text-white flex flex-row ml-auto mt-0">
+                    <img src={Menu}/>
                 </div>
             </div>
         </header>
